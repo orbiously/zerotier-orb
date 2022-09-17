0c29c227cacaa0397d20b12a5773d239e874fb84
Yann <yann.domingo@gmail.com>
2022-09-17 23:13:19 +0100

    fix: try to get proper message formatting

--- src/scripts/windows/setup.sh
+++ src/scripts/windows/setup.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 if (/c/progra~2/ZeroTier/One/zerotier-cli.bat -v) > NUL 2>&1; then
-  echo "ZeroTier CLI v.$(/c/progra~2/ZeroTier/One/zerotier-cli.bat -v) for Windows is already installed"
+  VERSION=$(/c/progra~2/ZeroTier/One/zerotier-cli.bat -v)
+  echo "ZeroTier CLI v.$VERSION for Windows is already installed"
 else
   echo "Installing ZeroTier CLI for Windows"
   curl "https://download.zerotier.com/dist/ZeroTier%20One.msi" -o zerotier-one.msi
