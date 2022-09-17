736d0006def1437f54c2a9d9b8a46261475dbc0a
Yann <yann.domingo@gmail.com>
2022-09-17 23:15:41 +0100

    fix: maybe with escaping

--- src/scripts/windows/setup.sh
+++ src/scripts/windows/setup.sh
@@ -2,7 +2,7 @@
 
 if (/c/progra~2/ZeroTier/One/zerotier-cli.bat -v) > NUL 2>&1; then
   VERSION=$(/c/progra~2/ZeroTier/One/zerotier-cli.bat -v)
-  echo "ZeroTier CLI v.$VERSION for Windows is already installed"
+  echo "ZeroTier CLI v\.$VERSION for Windows is already installed"
 else
   echo "Installing ZeroTier CLI for Windows"
   curl "https://download.zerotier.com/dist/ZeroTier%20One.msi" -o zerotier-one.msi
