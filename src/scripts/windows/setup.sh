b0d0a458326757ad49f06ae590b733bfa13cab34
Yann <yann.domingo@gmail.com>
2022-09-18 00:17:33 +0100

    syle: still that one message

--- src/scripts/windows/setup.sh
+++ src/scripts/windows/setup.sh
@@ -2,8 +2,7 @@
 
 if (/c/progra~2/ZeroTier/One/zerotier-cli.bat -v) > NUL 2>&1; then
   printf "\nZeroTier CLI for Windows is already installed"
-  printf "\nInstalled version: v.%s\n" "$(/c/progra~2/ZeroTier/One/zerotier-cli.bat -v)\n"
-  printf "\nZeroTier CLI v.%s for Windows is already installed\n" "$(/c/progra~2/ZeroTier/One/zerotier-cli.bat -v)"
+  printf "\nInstalled version: v.%s" "$(/c/progra~2/ZeroTier/One/zerotier-cli.bat -v)\n"
 else
   echo "Installing ZeroTier CLI for Windows"
   curl "https://download.zerotier.com/dist/ZeroTier%20One.msi" -o zerotier-one.msi
