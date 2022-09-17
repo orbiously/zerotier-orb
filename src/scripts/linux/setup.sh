a35d994b218ee82fbd5552538126ead09b5716c6
Yann <yann.domingo@gmail.com>
2022-09-18 00:27:10 +0100

    refactor: everything to dev/null

--- src/scripts/linux/setup.sh
+++ src/scripts/linux/setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if ( zerotier-cli -v ) 2>/dev/null; then
+if ( zerotier-cli -v ) > /dev/null 2>&1; then
   echo "ZeroTier CLI v.$(zerotier-cli -v) for Linux is already installed"
 else
   echo "Installing ZeroTier CLI for Linux"
