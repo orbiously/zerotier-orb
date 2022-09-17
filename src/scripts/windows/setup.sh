d6e4c0cd9a59bbdcdf00745f379a400d593a4368
Yann <yann.domingo@gmail.com>
2022-09-18 00:11:23 +0100

    fix: shellcheck

--- src/scripts/windows/setup.sh
+++ src/scripts/windows/setup.sh
@@ -2,7 +2,7 @@
 
 if (/c/progra~2/ZeroTier/One/zerotier-cli.bat -v) > NUL 2>&1; then
   printf "\nZeroTier CLI for Windows is already installed"
-  printf "Installed version: v.$(/c/progra~2/ZeroTier/One/zerotier-cli.bat -v)\n"
+  printf "Installed version: v.%s\n" "$(/c/progra~2/ZeroTier/One/zerotier-cli.bat -v)"
 else
   echo "Installing ZeroTier CLI for Windows"
   curl "https://download.zerotier.com/dist/ZeroTier%20One.msi" -o zerotier-one.msi
