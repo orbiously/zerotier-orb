faa36772734d9ea1ec66461aaf1b867e5d31a2f3
Yann <yann.domingo@gmail.com>
2022-09-18 00:24:56 +0100

    style: final layout for that silly message

--- src/scripts/windows/setup.sh
+++ src/scripts/windows/setup.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 if (/c/progra~2/ZeroTier/One/zerotier-cli.bat -v) > NUL 2>&1; then
-  printf "\nZeroTier CLI for Windows is already installed"
-  printf "\nInstalled version: v.%s" "$(/c/progra~2/ZeroTier/One/zerotier-cli.bat -v)\n"
+  printf "\nZeroTier CLI for Windows is already installed\n"
+  echo "Installed version: v.$(/c/progra~2/ZeroTier/One/zerotier-cli.bat -v)"
 else
   echo "Installing ZeroTier CLI for Windows"
   curl "https://download.zerotier.com/dist/ZeroTier%20One.msi" -o zerotier-one.msi
