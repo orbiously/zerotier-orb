835d65e9bf4b07bec8789ac71f6e55c0d6e203c8
Yann <yann.domingo@gmail.com>
2022-09-19 00:57:11 +0100

    fix: fix path to .bat

--- src/scripts/windows/disconnect.sh
+++ src/scripts/windows/disconnect.sh
@@ -5,7 +5,7 @@ echo "Leaving ZeroTier network"
 
 if [ "$PARAM_STORE_LOG" = 1 ]; then
   printf "\nGenerating ZeroTier log\n\n"
-  c/progra~2/ZeroTier/One/zerotier-cli.bat dump
+  /c/progra~2/ZeroTier/One/zerotier-cli.bat dump
   cp "$CIRCLE_WORKING_DIRECTORY\..\Desktop\zerotier_dump.txt" /c/tmp
 fi
 
