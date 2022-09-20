6e6ee7cf0088a56e967978dded9107b339e33a3d
Yann <yann.domingo@gmail.com>
2022-09-20 01:08:38 +0100

    fix: retrieve ZeroTier log from dump folder

--- src/scripts/macos/disconnect.sh
+++ src/scripts/macos/disconnect.sh
@@ -5,7 +5,8 @@ sudo zerotier-cli leave "$NETWORK_ID"
 
 if [ "$PARAM_STORE_LOG" = 1 ]; then
   printf "\nGenerating ZeroTier log\n\n"
-  cd /tmp && sudo zerotier-cli dump
+  sudo zerotier-cli dump
+  cp /Users/distiller/Desktop/zerotier_dump.txt /tmp
 fi
 
 echo "Stopping ZeroTier service"
